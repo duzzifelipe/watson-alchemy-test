65a04a996e7b31fbad66e379d2bfa7b8947e06d3
Felipe Duzzi <duzzifelipe@gmail.com>
2016-12-26 13:32:55 -0200

    text to speech

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 require('dotenv').config({ silent: true });
 var requireEnv = require('require-environment-variables');
 var readline = require('readline');
+var fs = require('fs');
+var play = require('play');
 var AlchemyLanguageV1 = require('watson-developer-cloud/alchemy-language/v1');
+var TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
 
 requireEnv(['API_KEY']);
 
 var alchemy_language = new AlchemyLanguageV1({
     api_key: process.env.API_KEY
 });
+
+// var text_to_speech = new TextToSpeechV1({
+//     api_key: process.env.API_KEY
+// });
 
 var rl = readline.createInterface({
     input: process.stdin,
@@ -24,5 +31,16 @@ rl.question('Enter something for Watson: ', function(answer) {
             console.log(JSON.stringify(response, null, 2));
     });
 
+    /*
+    text_to_speech.synthesize({
+        text: answer,
+        accept: 'audio/wav',
+        voice: 'pt-BR_IsabelaVoice'
+    }).pipe(fs.createWriteStream('./output.wav'));
+    play.sound('./output.wav');
+    */
+
     rl.close();
 });
+
+
